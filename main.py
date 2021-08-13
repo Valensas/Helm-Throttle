bf1dedf5a976612d418bae16da4fd4da7b5301d8
Okan Okumusoglu <okanokumusoglu@akins\032macbook\032pro.bitronit.com>
2021-08-13 15:04:53 +0300

    Formatting

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from sys import stdout
-from typing import SupportsRound
-from flask import Flask, Response
 from flask import request
 import time
-from flask.scaffold import F
 import requests
-from requests.api import get, head
+from flask import Flask, Response
 import re
 from optparse import OptionParser
+
 app = Flask(__name__)
 parser = OptionParser()
 parser.add_option(
@@ -15,55 +12,57 @@ parser.add_option(
     dest="interval",
     type="int",
     default=5,
-    help="write report to FILE",
-    metavar="FILE",
+    help="Enter a time-interval",
 )
 parser.add_option(
     "--kube-api",
     dest="kube_api",
     type="str",
-    default="https://rancher.valensas.com/k8s/clusters/c-dvgp5",
-    help="don't print status messages to stdout",
+    default="",
+    help="Enter an url",
 )
 (options, args) = parser.parse_args()
-kubeApiURL = options.kube_api
-second = {"prevProcessMilisec": -1}
+kube_api_URL = options.kube_api
+time_interval = options.interval
+last_write_request_throttled_timestamp = None
+regex_list = [
+    "^apis\/apps\/v1\/namespaces\/[^\/]+\/statefulsets(\/[^\/]+)?$",
+    "^apis\/apps\/v1\/namespaces\/[^\/]+\/deployments(\/[^\/]+)?$",
+]
+
+
+def check_for_throttle(path):
+    method_type = request.method
+    is_method_type_matched = (
+        method_type == "POST" or method_type == "PUT" or method_type == "PATCH"
+    )
+    is_regex_matched = any(re.search(element, path) for element in regex_list)
+    is_dry_run = request.args.get("dryRun") == "All"
+    return is_method_type_matched and is_regex_matched and not is_dry_run
+
+
 @app.route(
     "/", defaults={"path": ""}, methods=["POST", "GET", "PUT", "PATCH", "DELETE"]
 )
 @app.route("/<string:path>", methods=["POST", "GET", "PUT", "PATCH", "DELETE"])
 @app.route("/<path:path>", methods=["POST", "GET", "PUT", "PATCH", "DELETE"])
 def _proxy(path):
-    statefulRegex = "^apis\/apps\/v1\/namespaces\/[^\/]+\/statefulsets(\/[^\/]+)?$"
-    deploymentRegex = "^apis\/apps\/v1\/namespaces\/[^\/]+\/deployments(\/[^\/]+)?$"
-    methodType = request.method
-    isDryRun = request.args.get("dryRun") == "All"
-    if methodType == "POST" or methodType == "PUT" or methodType == "PATCH":
-        if (
-            re.search(statefulRegex, path) is not None
-            or re.search(deploymentRegex, path)
-        ) and not isDryRun:
-            if second["prevProcessMilisec"] != -1:
-                milli_sec = int(round(time.time() * 1000))
-                timeDif = milli_sec - second["prevProcessMilisec"]
-                dif = (options.interval * 1000 - timeDif) / 1000.0
-                print("Time dif: ", timeDif)
-                print("Milli sec: ", milli_sec % 100000)
-                print("Dif: ", dif)
-                print("Prev milisec: ", second["prevProcessMilisec"] % 100000)
-                if dif > 0:
-                    print("Sleeping!")
-                    time.sleep(dif)
-                    print("Sleeping over!")
-                    print(
-                        "================================================================================================="
-                    )
-            second["prevProcessMilisec"] = int(round(time.time() * 1000))
+    global last_write_request_throttled_timestamp
+    if check_for_throttle(path=path):
+        if last_write_request_throttled_timestamp is not None:
+            milli_sec = int(round(time.time() * 1000))
+            timeDif = milli_sec - last_write_request_throttled_timestamp
+            dif = (time_interval * 1000 - timeDif) / 1000.0
+            if dif > 0:
+                time.sleep(dif)
+        last_write_request_throttled_timestamp = int(
+            round(time.time() * 1000))
     resp = requests.request(
         params=request.args,
         method=request.method,
-        url=kubeApiURL + "/" + path,
-        headers={key: value for (key, value) in request.headers if key != "Host"},
+        url=kube_api_URL + "/" + path,
+        headers={key: value for (key, value)
+                 in request.headers if key != "Host"},
         data=request.get_data(),
         cookies=request.cookies,
         allow_redirects=False,
@@ -81,5 +80,7 @@ def _proxy(path):
     ]
     response = Response(resp.content, resp.status_code, headers)
     return response
+
+
 if __name__ == "__main__":
     app.run(debug=True, ssl_context="adhoc")
