35cec948c7c07f87f370451d9d9f27312263b299
Moray Baruh <moray@valensas.com>
2021-08-19 20:23:47 +0300

    refactor and support for certificate settings

--- helm-throttle.sh
+++ helm-throttle.sh
@@ -3,8 +3,6 @@
 if [ "$HELM_DEBUG" "=" "true" ]; then
   set -x
   export DEBUG="1"
-else
-  REDIRECT=""
 fi
 
 set -eu
@@ -12,8 +10,8 @@ set -eu
 trap finish EXIT
 function finish {
   set +eu
-  if test -z "$OUT";then
-    rm $OUT
+  if test -d "$TMP_DIR" ;then
+    rm -rf "$TMP_DIR"
   fi
   kill %
 }
@@ -22,14 +20,22 @@ KUBECONTEXT=${HELM_KUBECONTEXT:-""}
 KUBECONFIG=${KUBECONFIG:-~/.kube/config}
 
 source ${HELM_PLUGIN_DIR}/venv/bin/activate
+TMP_DIR="$(mktemp -d)"
 
-export KUBEAPI=$(python3 $HELM_PLUGIN_DIR/find-kube-server.py --kube-config="${KUBECONFIG}"  --kube-context="${KUBECONTEXT}")
+export KUBEAPI=${HELM_KUBEAPISERVER:-$(python3 $HELM_PLUGIN_DIR/find-kube-property.py server                   --kube-config="${KUBECONFIG}" --kube-context="${KUBECONTEXT}" -o "${TMP_DIR}")}
+export INSECURE_SKIP_TLS_VERIFY=$(python3      $HELM_PLUGIN_DIR/find-kube-property.py insecure-skip-tls-verify --kube-config="${KUBECONFIG}" --kube-context="${KUBECONTEXT}" -o "${TMP_DIR}")
+export CA_CERT=$(python3                       $HELM_PLUGIN_DIR/find-kube-property.py certificate-authority    --kube-config="${KUBECONFIG}" --kube-context="${KUBECONTEXT}" -o "${TMP_DIR}")
+export CLIENT_CERT=$(python3                   $HELM_PLUGIN_DIR/find-kube-property.py client-certificate       --kube-config="${KUBECONFIG}" --kube-context="${KUBECONTEXT}" -o "${TMP_DIR}")
+export CLIENT_KEY=$(python3                    $HELM_PLUGIN_DIR/find-kube-property.py client-key               --kube-config="${KUBECONFIG}" --kube-context="${KUBECONTEXT}" -o "${TMP_DIR}")
 export TIME_INTERVAL=5
 
 python3 $HELM_PLUGIN_DIR/throttle-proxy.py &
 ${HELM_PLUGIN_DIR}/wait-for-it.sh -h 127.0.0.1 -p 5000 -t 5 --quiet
-OUT="$(mktemp)"
-python3 $HELM_PLUGIN_DIR/gen_kubeconfig.py --kube-config="${KUBECONFIG}"  --kube-context="${KUBECONTEXT}" -o "${OUT}"
+NEW_KUBECONFIG="$TMP_DIR/kubeconfig"
+touch "${NEW_KUBECONFIG}"
+chmod 600 "${NEW_KUBECONFIG}"
+python3 $HELM_PLUGIN_DIR/gen-kubeconfig.py --kube-config="${KUBECONFIG}"  --kube-context="${KUBECONTEXT}" -o "${NEW_KUBECONFIG}"
 
-helm $* --kubeconfig="${OUT}"
-
+export KUBECONFIG="${NEW_KUBECONFIG}"
+unset HELM_KUBEAPISERVER
+helm $*
--- throttle-proxy.py
+++ throttle-proxy.py
@@ -1,63 +1,55 @@
-from concurrent.futures import ThreadPoolExecutor
-from flask import request
-import time
-import requests
-from flask import Flask, Response
-import re
-import os
 import logging
+import os
+import re
+import time
+import warnings
+from concurrent.futures import ThreadPoolExecutor
+
+from flask import Flask, Response
+from flask import request
+from requests import Session
+from urllib3.exceptions import InsecureRequestWarning
 
 app = Flask(__name__)
 
 kube_api_url = os.environ.get("KUBEAPI")
 if kube_api_url is None:
     raise Exception("URL Not Given!")
+kube_api_url = kube_api_url.removesuffix('/')
 
-time_interval = int(os.environ.get("TIME_INTERVAL", 10))
+throttle_interval = int(os.environ.get("THROTTLE_INTERVAL", 10))
 
-last_write_request_throttled_timestamp = None
+last_write_timestamp = None
 
 regex_list = [
-    "^apis\/apps\/v1\/namespaces\/[^\/]+\/statefulsets(\/[^\/]+)?$",
-    "^apis\/apps\/v1\/namespaces\/[^\/]+\/deployments(\/[^\/]+)?$",
+    "^/apis/apps/v1/namespaces/[^/]+/statefulsets(/[^/]+)?$",
+    "^/apis/apps/v1/namespaces/[^/]+/deployments(/[^/]+)?$",
 ]
 
-
-def check_for_throttle(path):
-    method_type = request.method
-    is_method_type_matched = (
-        method_type == "POST" or method_type == "PUT" or method_type == "PATCH"
-    )
-    is_regex_matched = any(re.search(element, path) for element in regex_list)
-    is_dry_run = request.args.get("dryRun") == "All"
-    return is_method_type_matched and is_regex_matched and not is_dry_run
-
-
 executor = ThreadPoolExecutor(1)
+session = Session()
 
 
-@app.route(
-    "/", defaults={"path": ""}, methods=["POST", "GET", "PUT", "PATCH", "DELETE"]
-)
-@app.route("/<string:path>", methods=["POST", "GET", "PUT", "PATCH", "DELETE"])
-@app.route("/<path:path>", methods=["POST", "GET", "PUT", "PATCH", "DELETE"])
-def _proxy(path):
-    global last_write_request_throttled_timestamp
-    if check_for_throttle(path=path):
-        if last_write_request_throttled_timestamp is not None:
-            milli_sec = int(round(time.time() * 1000))
-            timeDif = milli_sec - last_write_request_throttled_timestamp
-            dif = (time_interval * 1000 - timeDif) / 1000.0
-            if dif > 0:
-                executor.submit(time.sleep, (dif)).result()
-        last_write_request_throttled_timestamp = int(
-            round(time.time() * 1000))
-    resp = requests.request(
+def is_write():
+    return request.method == "POST" or request.method == "PUT" or request.method == "PATCH"
+
+
+def should_throttle(path):
+    global last_write_timestamp
+    if last_write_timestamp is None:
+        return False
+
+    resource_is_listed = any(re.search(element, path) for element in regex_list)
+    is_dry_run = request.args.get("dryRun") == "All"
+    return is_write() and resource_is_listed and not is_dry_run
+
+
+def proxy_request():
+    resp = session.request(
         params=request.args,
         method=request.method,
-        url=kube_api_url + "/" + path,
-        headers={key: value for (key, value)
-                 in request.headers if key != "Host"},
+        url=kube_api_url + request.path,
+        headers={key: value for (key, value) in request.headers if key.lower() != "host"},
         data=request.get_data(),
         cookies=request.cookies,
         allow_redirects=False,
@@ -73,14 +65,47 @@ def _proxy(path):
         for (name, value) in resp.raw.headers.items()
         if name.lower() not in excluded_headers
     ]
-    response = Response(resp.content, resp.status_code, headers)
-    return response
+    return Response(resp.content, resp.status_code, headers)
 
 
-if os.environ.get('DEBUG') is None:
+if os.environ.get('CA_CERT') and os.environ.get('CA_CERT') != '':
+    session.verify = os.environ.get('CA_CERT')
+
+if os.environ.get('INSECURE_SKIP_TLS_VERIFY') and os.environ.get('INSECURE_SKIP_TLS_VERIFY').lower() == 'true':
+    session.verify = False
+    # Disable SSL Warnings
+    warnings.simplefilter('ignore', InsecureRequestWarning)
+
+if os.environ.get('CLIENT_CERT') and os.environ.get('CLIENT_CERT') != '':
+    session.cert = (os.environ.get('CLIENT_CERT'), os.environ.get('CLIENT_KEY'))
+
+@app.route(
+    "/", defaults={"path": ""}, methods=["POST", "GET", "PUT", "PATCH", "DELETE"]
+)
+@app.route("/<string:_>", methods=["POST", "GET", "PUT", "PATCH", "DELETE"])
+@app.route("/<path:_>", methods=["POST", "GET", "PUT", "PATCH", "DELETE"])
+def _proxy(_):
+    global last_write_timestamp
+    if should_throttle(path=request.path):
+        current_timestamp = int(round(time.time() * 1000))
+        time_since_last_throttle = current_timestamp - last_write_timestamp
+        throttle_time = (throttle_interval * 1000 - time_since_last_throttle) / 1000.0
+        if throttle_time > 0:
+            executor.submit(time.sleep, throttle_time).result()
+    if is_write():
+        last_write_timestamp = int(round(time.time() * 1000))
+
+    return proxy_request()
+
+
+def disable_logs():
     log = logging.getLogger('werkzeug')
     log.setLevel(logging.WARNING)
     os.environ['WERKZEUG_RUN_MAIN'] = 'true'
+
+
+if os.environ.get('DEBUG') is None:
+    disable_logs()
 
 if __name__ == "__main__":
     app.run(debug=False, ssl_context="adhoc")
